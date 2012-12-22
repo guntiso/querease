8f991b32b17e577823e7e4adb6e1e307bd3d527e
Martins Rumkovskis <martins@uniso.lv>
2012-12-23 01:19:23 +0200

    query support initial commit

--- src/main/scala/kpsws/ort.scala
+++ src/main/scala/kpsws/ort.scala
@@ -2,10 +2,17 @@ package kpsws
 
 import org.tresql.ORT
 import org.tresql.NameMap
+import org.tresql.Query
 import xsdgen.ElementName
-
 import scala.collection.JavaConversions._
 import javax.xml.datatype._
+import kpsws.impl._
+
+/** TODO This is duplicated code from file XsdGen.scala */
+case class XsdTypeDef(
+  name: String,
+  table: String,
+  fields: Seq[XsdTypeDef])
 
 object ort extends org.tresql.NameMap {
   
@@ -27,34 +34,32 @@ object ort extends org.tresql.NameMap {
       val propClass = propToClassName(propName) //find class name in the case value is map or list i.e. not primitive object
       val t = m.getParameterTypes()(0)
       map.get(propName).map {
-        _ match {
-          //this may require tuning since it is difficult with java reflection to determine compatibility
-          //between map entry value type and setter parameter type
-          //hopefully scala reflection will help with this
-          case d: BigDecimal => {
-            if (t == classOf[Double] || t == classOf[java.lang.Double]) m.invoke(pojo,
-              d.doubleValue.asInstanceOf[Object])
-            else if (t == classOf[java.math.BigDecimal]) m.invoke(pojo, d.bigDecimal)
-            else if (t == classOf[java.math.BigInteger]) m.invoke(pojo, d.bigDecimal.unscaledValue)
-            else m.invoke(pojo, d)
-          }
-          case x if (t == classOf[java.math.BigInteger] && x != null) => m.invoke(pojo,
-            new java.math.BigInteger(x.toString))
-          case x:java.util.Date if (t == classOf[XMLGregorianCalendar]) => {
-            val gc = new java.util.GregorianCalendar()
-            gc.setTime(x)
-            m.invoke(pojo, XML_DATATYPE_FACTORY.newXMLGregorianCalendar(gc))
-          }
-          case inMap: Map[String, _] if (t == Class.forName(propClass)) => m.invoke(pojo, mapToPojo(inMap,
-              Class.forName(propClass).newInstance()).asInstanceOf[Object])
-          //may be exact collection which is used in xsd generated pojos must be used?
-          case Seq() if (classOf[java.util.Collection[_]].isAssignableFrom(t)) => t.newInstance
-          case s:Seq[Map[String, _]] if (classOf[java.util.Collection[_]].isAssignableFrom(t)) => {
-            val col:java.util.Collection[_] = s.map(mapToPojo(_, Class.forName(propClass).newInstance()))
-            m.invoke(pojo, col)
-          }
-          case x => m.invoke(pojo, x.asInstanceOf[Object])
+        //this may require tuning since it is difficult with java reflection to determine compatibility
+        //between map entry value type and setter parameter type
+        //hopefully scala reflection will help with this
+        case d: BigDecimal => {
+          if (t == classOf[Double] || t == classOf[java.lang.Double]) m.invoke(pojo,
+            d.doubleValue.asInstanceOf[Object])
+          else if (t == classOf[java.math.BigDecimal]) m.invoke(pojo, d.bigDecimal)
+          else if (t == classOf[java.math.BigInteger]) m.invoke(pojo, d.bigDecimal.unscaledValue)
+          else m.invoke(pojo, d)
         }
+        case x if (t == classOf[java.math.BigInteger] && x != null) => m.invoke(pojo,
+          new java.math.BigInteger(x.toString))
+        case x: java.util.Date if (t == classOf[XMLGregorianCalendar]) => {
+          val gc = new java.util.GregorianCalendar()
+          gc.setTime(x)
+          m.invoke(pojo, XML_DATATYPE_FACTORY.newXMLGregorianCalendar(gc))
+        }
+        case inMap: Map[String, _] if (t == Class.forName(propClass)) => m.invoke(pojo, mapToPojo(inMap,
+          Class.forName(propClass).newInstance).asInstanceOf[Object])
+        //may be exact collection which is used in xsd generated pojos must be used?
+        case Seq() if (classOf[java.util.Collection[_]].isAssignableFrom(t)) => t.newInstance
+        case s: Seq[Map[String, _]] if (classOf[java.util.Collection[_]].isAssignableFrom(t)) => {
+          val col: java.util.Collection[_] = s.map(mapToPojo(_, Class.forName(propClass).newInstance))
+          m.invoke(pojo, col)
+        }
+        case x => m.invoke(pojo, x.asInstanceOf[Object])
       }
     }
     pojo
@@ -84,6 +89,35 @@ object ort extends org.tresql.NameMap {
     ORT.save(tableName,
       pojoToMap(pojo).map(e => (xsdNameToDbName(e._1), e._2)) + ("id" -> id))
 
+  /* -------- Query support methods -------- */
+
+  def query(view:XsdTypeDef, pojoClass:Class[_], params:ListRequestType) = {
+    Query.select(queryString(view, params), values(params.Filter)).toListRowAsMap.map(mapToPojo(_, pojoClass.newInstance))
+  }
+      
+  private def queryString(view:XsdTypeDef, params:ListRequestType) = from(view) + 
+   where(view.name, params.Filter) + cols(view) + sort(view.name, params.Sort)
+      
+  private def fn(baseView:String, name: String) = name.split("\\.") match {
+      case Array(c) => tableName(baseView) + "." + colName(baseView, c)
+      case Array(v, c) => tableName(v) + "." + colName(v, c)
+  } 
+  
+  private def cols(view:XsdTypeDef) = view.fields.map(f=> f.table + "." + f.name ).mkString(" {", ", ", "}")
+  
+  private def from(view:XsdTypeDef) =
+    view.fields.foldLeft(scala.collection.mutable.Set[String]())(_ += _.table).mkString("/") 
+  
+  private def where(baseView:String, filter: Array[ListFilterType]) = filter.map(f=>
+    fn(baseView, f.Field) + " " + f.Comparison + " ?").mkString(" & ")
+    
+  private def values(filter: Array[ListFilterType]) = filter.map(_.Value)
+    
+  private def sort(baseView:String, sort:Array[ListSortType]) = sort.map(s=>
+    (if(s.Order == "desc") "~" else "") + "#(" + fn(baseView, s.Field) + ")")
+    
+  //TODO support limit, offset
+    
   def db_ws_name_map(ws: Map[String, _]) = ws.map(t => t._1.toLowerCase -> t._1)
 
 }
