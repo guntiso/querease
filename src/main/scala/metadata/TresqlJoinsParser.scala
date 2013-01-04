700bd30d0d9adc7e1505ec8c6045288853de7c34
Martins Rumkovskis <martins@uniso.lv>
2013-01-05 00:39:09 +0200

    query string generation refactoring and fixes

--- src/main/scala/metadata/TresqlJoinsParser.scala
+++ src/main/scala/metadata/TresqlJoinsParser.scala
@@ -3,11 +3,11 @@ package xsdgen
 import org.tresql.QueryParser._
 
 object JoinsParser {
-  def aliasToTableMap(joins: String): Map[String, String] = if (joins == null) Map() else {
+  def apply(joins: String): Map[String, String] = if (joins == null) Map() else {
     parseExp(joins) match {
       case Query(tables, _, _, _, _, _, _, _) => (tables flatMap {
         // primary key join: customer c[c.m_id m, c.f_id f]person  
-        case Obj(Ident(name), null, Join(false, Arr(jl), _), _) if (jl.size > 1) => (jl map {
+        case Obj(Ident(name), null, Join(false, Arr(jl), _), _) => (jl map {
           case Obj(Ident(_), al, _, _) if al != null => al -> name.mkString(".")
           case _ => (null, null)
         }).filter(_._1 != null)
