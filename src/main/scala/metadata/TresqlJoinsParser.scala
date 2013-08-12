b6f63b1555edc9fabe9253be9c909ebc34305dee
Martins Rumkovskis <martins@uniso.lv>
2013-08-12 23:06:55 +0300

    upgrade to tresql 3.11

--- src/main/scala/metadata/TresqlJoinsParser.scala
+++ src/main/scala/metadata/TresqlJoinsParser.scala
@@ -20,17 +20,17 @@ object JoinsParser {
       case Query(tables, _, _, _, _, _, _, _) => (tables.foldLeft(List[Join]()) { (joins, j) =>
         j match {
           //base table
-          case Obj(Ident(name), alias, null, outerJoin) =>
+          case Obj(Ident(name), alias, null, outerJoin, _) =>
             val n = name.mkString(".")
             currentJoin = Join(alias, n, if (n == baseTable) Right(outerJoin != null) else Left(n))
             fillJoinMap(currentJoin)
             currentJoin :: joins
           //foreign key alias join or normal join without alias
-          case Obj(Ident(name), null, QPJoin(false, Arr(jl), false), oj1) => jl.foldLeft(joins) {
+          case Obj(Ident(name), null, QPJoin(false, Arr(jl), false), oj1, _) => jl.foldLeft(joins) {
             (joins, j) =>
               j match {
                 // foreign key alias join: customer c[c.m_id m, c.f_id f]person
-                case Obj(Ident(_), alias, _, oj2) =>
+                case Obj(Ident(_), alias, _, oj2, _) =>
                   val join = Join(alias, name.mkString("."), nullable(oj1 != null || oj2 != null))
                   fillJoinMap(join)
                   join :: joins
@@ -43,13 +43,13 @@ object JoinsParser {
               }
           }
           //alias join i.e. no join
-          case Obj(Ident(name), null, QPJoin(false, null, true), null) =>
+          case Obj(Ident(name), null, QPJoin(false, null, true), null, _) =>
             val n = name.mkString(".")
             currentJoin = joinMap.getOrElse(n, Join(null, n, Left(n)))
             joins
           // normal join with alias: customer c[c.person_id = p.id]person p
           // default join: customer c/person m
-          case Obj(Ident(name), alias, _, outerJoin) =>
+          case Obj(Ident(name), alias, _, outerJoin, _) =>
             val join = Join(alias, name.mkString("."), nullable(outerJoin != null))
             fillJoinMap(join)
             currentJoin = join
