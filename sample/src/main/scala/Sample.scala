9017ff792ca6be108a405f1a2dc485011fe8eaa0
Guntis Ozols <guntiso@uniso.lv>
2014-05-04 00:20:01 +0300

    sample - improvements and fixes, dialect support

--- sample/src/main/scala/Sample.scala
+++ sample/src/main/scala/Sample.scala
@@ -1,6 +1,8 @@
+import java.sql.Connection
 import java.sql.DriverManager
 
 import org.tresql.Env
+import org.tresql.dialects._
 
 import com.typesafe.config.ConfigFactory
 
@@ -11,13 +13,14 @@ import mojoz.metadata.out._
 import querease._
 
 object Sample {
-  def main(args: Array[String]) {
     val yamlMd = YamlMd.fromResources()
     val tableMd = new Metadata(new YamlTableDefLoader(yamlMd).tableDefs)
     val viewDefs = (new YamlViewDefLoader(tableMd, yamlMd) with TresqlJoinsParser).viewDefs
     val i18nRules = I18nRules.suffixI18n(Set("_eng", "_rus"))
     val md = new Metadata(tableMd.tableDefs, viewDefs, i18nRules)
-    val qe = new ScalaDtoQuerease(md) with Querease
+    val qio = QuereaseIo.scalaDto(md)
+    val builder = QueryStringBuilder.default(md.extendedViewDef.get)
+    val qe = new Querease(qio, builder)
 
     println("Entities:")
     md.tableDefs.map(_.name) foreach println
@@ -31,22 +34,36 @@ object Sample {
     val url = conf.getString("sample.jdbc.url")
     val usr = conf.getString("sample.db.user")
     val pwd = conf.getString("sample.db.password")
+    val dialect = conf.getString("sample.db.dialect")
     val dbg = conf.getBoolean("sample.db.statement.debug")
-    val conn = DriverManager.getConnection(url, usr, pwd)
+
+  def getConnection = DriverManager.getConnection(url, usr, pwd)
+  def setEnv(conn: Connection = getConnection) = {
     conn.setAutoCommit(false)
+    Env.dialect = Sample.dialect match {
+      case "oracle" => OracleDialect
+      case "hsql" => HSQLDialect
+      case "ansi" | "" | null => ANSISQLDialect
+    }
     Env.metaData = new TresqlMetadata(md.tableDefs, null)
     Env.conn = conn
     if (dbg) Env update { (msg, level) => println(msg) }
     else Env update { (msg, level) => () }
+  }
+  def clearEnv = {
+    if (Env.conn != null) Env.conn.close
+    Env.conn = null
+  }
 
+  def main(args: Array[String]) {
+    setEnv()
     try {
-      val banks = qe.query(classOf[BankListRow], null)
+      val banks = qe.list(classOf[BankListRow], null, 0, 10, null)
       println("Bank codes:")
       banks.map(_.code) foreach println
       println
     } finally {
-      Env.conn = null
-      conn.close
+      clearEnv
     }
   }
 }
