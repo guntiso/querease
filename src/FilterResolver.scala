ce0689eacac86d1616abbafe3943d558caa0706a
Guntis Ozols <guntiso@uniso.lv>
2017-09-05 06:30:31 +0300

    filter resolver - support for field refs improved

--- src/FilterResolver.scala
+++ src/FilterResolver.scala
@@ -1,13 +1,47 @@
 package querease;
 
 import mojoz.metadata._
+import org.tresql.{ CacheBase, SimpleCacheBase }
+import org.tresql.parsing.{ QueryParsers, ExpTransformer }
+import scala.util.parsing.input.CharSequenceReader
 
+object FilterTransformer {
+  trait Parser extends QueryParsers with ExpTransformer {
+    def parse(expr: String): Exp
+  }
+  abstract class DefaultParser extends Parser {
+    val cache: Option[CacheBase[Exp]]
+    override def ident: MemParser[String] = opt("^") ~ super.ident ^^ {
+      case Some(s) ~ ident => s + ident
+      case None ~ ident => ident
+    }  named "^ident"
+    override def parse(expr: String): Exp = {
+      cache.flatMap(_.get(expr)).getOrElse {
+        try {
+          intermediateResults.get.clear
+          val e = phrase(exprList)(new CharSequenceReader(expr)) match {
+            case Success(r, _) => r
+            case x => sys.error(x.toString)
+          }
+          cache.map(_.put(expr, e))
+          e
+        } finally intermediateResults.get.clear
+      }
+    }
+  }
+  object DefaultParser extends DefaultParser {
+    override val cache = Some(new SimpleCacheBase[Exp](4096))
+  }
+}
+
+import FilterTransformer._
 trait FilterTransformer {
+  val parser: Parser = DefaultParser
   // TODO resolve names like in views (maybe strip prefix etc.)
   private val ident = "[_\\p{IsLatin}][_\\p{IsLatin}0-9]*"
   private val ident2 = s"$ident\\.$ident"
   private val qualifiedIdent = s"$ident(\\.$ident)*"
-  private val fieldRef = "\\^" + ident
+  private val qualifiedIdentOrRef = "\\^?" + qualifiedIdent
   private val s = "\\s*"
   private val any = ".*"
   private val Rq = "(!)?"
@@ -20,25 +54,41 @@ trait FilterTransformer {
     ("^" + patterns.map(toGroup).mkString(s, s, s) + "$").r
 
   private val IdentFilterDef =
-    regex(qualifiedIdent, Rq)
+    regex(qualifiedIdentOrRef, Rq)
   private val ComparisonFilterDef = regex(
-    qualifiedIdent, ComparisonOps, Rq)
+    qualifiedIdentOrRef, ComparisonOps, Rq)
   private val IntervalFilterDef = regex(Rq, IntervalOps.mkString("|"),
-    qualifiedIdent, IntervalOps.mkString("|"), Rq)
-  private val RefExpr = regex(fieldRef, any) // FIXME support ref[s] at any position
+    qualifiedIdentOrRef, IntervalOps.mkString("|"), Rq)
   // ! ChoiceRefExpr syntax is experimental
   private val ChoiceRefExpr = regex(qualifiedIdent, "=", choiceRef, Opt)
   private def opt(req: String) = if (req == "!") "" else "?"
   // TODO configure naming
   private def colName(name: String, baseTableAlias: String) =
-    if (baseTableAlias == null || name.indexOf(".") > 0) name
+    if (baseTableAlias == null || name.indexOf(".") > 0 || name.startsWith("^")) name
     else s"$baseTableAlias.$name"
   private def parName(name: String) =
-    (if (name.contains("_.")) name.substring(name.lastIndexOf("_.") + 2) else name)
+    (if (name.contains("_.")) name.substring(name.lastIndexOf("_.") + 2)
+     else if (name startsWith "^") name.substring(1)
+     else name)
       .replace(".", "_")
   def transformFilter(filter: String, view: ViewDef.ViewDefBase[FieldDef.FieldDefBase[Type]], baseTableAlias: String) = {
     def par(name: String) = parName(name)
-    def col(name: String) = colName(name, baseTableAlias)
+    def col(name: String) = resolveFieldRefs(colName(name, baseTableAlias))
+    def resolveFieldRefs(filterExpr: String): String = {
+      import parser._
+      transformer {
+        case iexpr @ Ident(List(ident)) =>
+          if (ident startsWith "^") {
+            val name = ident.substring(1)
+            Seq(view).filter(_ != null)
+              .flatMap(_.fields)
+              .find(f => Option(f.alias).getOrElse(f.name) == name)
+              .map(f => parse(Option(f.expression).getOrElse(
+                 Option(f.tableAlias).orElse(Option(baseTableAlias)).map(_ + ".").getOrElse("") + f.name)))
+              .getOrElse(iexpr)
+          } else iexpr
+      } (parse(filterExpr)).tresql
+    }
     filter match {
       case "true" | "false" =>
         filter
@@ -50,19 +100,10 @@ trait FilterTransformer {
         val nameFrom = par(name + "_from") // TODO configure naming
         val nameTo = par(name + "_to") // TODO configure naming
         s":$nameFrom${opt(reqFrom)} $opFrom ${col(name)} $opTo :$nameTo${opt(reqTo)}"
-      case RefExpr(refIdent, expr) =>
-        // very basic support for field ref
-        // FIXME support all shortcuts above
-        // FIXME support ref[s] at any position
-        val name = refIdent.substring(1)
-        view.fields
-          .find(f => Option(f.alias).getOrElse(f.name) == name)
-          .map(f => Option(f.expression).getOrElse(f.name) + expr)
-          .getOrElse(filter)
       case ChoiceRefExpr(name, _, _, choiceRef, opt) =>
         // TODO ChoiceRefExpr syntax is experimental, not implemented here yet
         s"/* TODO - CHOICE REF FOUND - $name, $choiceRef, $opt */true"
-      case x => x
+      case x => resolveFieldRefs(x)
     }
   }
 }
--- test/FilterResolverTests.scala
+++ test/FilterResolverTests.scala
@@ -1,5 +1,6 @@
 package test
 
+import mojoz.metadata._
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
@@ -7,13 +8,67 @@ import querease.FilterTransformer
 
 class FilterResolverTests extends FlatSpec with Matchers {
   private val Identifiers = "blah some_ident1".split("\\s+").toSet
+  private val FieldRefs = "^blah".split("\\s+").toSet
   private val IntervalOps = "< <=".split("\\s+").toSet
   private val ComparisonOps = "= < > <= >= != ~ ~~ !~ !~~".split("\\s+").toSet
   private val OtherFilters = Set("id = :id?", "a = b", "a = b & c = :d", "a<<")
 
   val transformer = new FilterTransformer {}
   def resolve(f: String, b: String = null) = transformer.transformFilter(f, null, b)
+  def resolveBlah(f: String, b: String = null) = transformer.transformFilter(f, viewBlah, b)
+  def resolveBlahA(f: String, b: String = null) = transformer.transformFilter(f, viewBlahA, b)
+  def resolveBlahExpr(f: String, b: String = null) = transformer.transformFilter(f, viewBlahExpr, b)
   private val b = "base"
+  private val f: FieldDef[Type] =
+    FieldDef(
+      table = null,
+      tableAlias = null,
+      name = null,
+      alias = null,
+      options = null,
+      isCollection = false,
+      maxOccurs = null,
+      isExpression = false,
+      expression = null,
+      saveTo = null,
+      resolver = null,
+      nullable = true,
+      default = null,
+      isForcedCardinality = false,
+      type_ = null,
+      enum = null,
+      joinToParent = null,
+      orderBy = null,
+      isI18n = false,
+      comments = null,
+      extras = null
+    )
+  private val v: ViewDef[FieldDef[Type]] =
+    ViewDef(
+      name = null,
+      table = null,
+      tableAlias = null,
+      joins = Nil, // from clause
+      filter = Nil, // where clause
+      groupBy = Nil,
+      having = Nil,
+      orderBy = Nil,
+      extends_  = null,
+      draftOf = null,
+      detailsOf = null,
+      comments = null,
+      fields = Nil,
+      saveTo = Nil,
+      extras = Map.empty
+    )
+  val fBlah = f.copy(name = "blah")
+  val fBlahA = f.copy(name = "blah", tableAlias = "ba")
+  val fBlahExpr = fBlah.copy(isExpression = true, expression = "to_date('1984')")
+  val viewBlah = v.copy(fields = Seq(fBlah))
+  val viewBlahA = v.copy(fields = Seq(fBlahA))
+  val viewBlahExpr = v.copy(fields = Seq(fBlahExpr))
+
+
 
   "filter sugar syntax" should "work properly for identifiers" in {
     Identifiers foreach { ident =>
@@ -28,6 +83,61 @@ class FilterResolverTests extends FlatSpec with Matchers {
       resolve("true") should be("true")
       resolve("false") should be("false")
     }
+  }
+
+  "filter sugar syntax" should "work properly for missing field refs" in {
+    FieldRefs foreach { fieldRef =>
+      val ident = fieldRef.substring(1)
+      resolve(fieldRef) should be(s"$fieldRef = :$ident?")
+      resolve(s"$fieldRef!") should be(s"$fieldRef = :$ident")
+      resolve(s"$fieldRef !") should be(s"$fieldRef = :$ident")
+      //
+      resolve(fieldRef, b) should be(s"$fieldRef = :$ident?")
+      resolve(s"$fieldRef!", b) should be(s"$fieldRef = :$ident")
+      resolve(s"$fieldRef !", b) should be(s"$fieldRef = :$ident")
+    }
+  }
+
+  "filter sugar syntax" should "work properly for valid field refs" in {
+    FieldRefs foreach { fieldRef =>
+      val ident = fieldRef.substring(1)
+      resolveBlah(fieldRef) should be(s"$ident = :$ident?")
+      resolveBlah(s"$fieldRef!") should be(s"$ident = :$ident")
+      resolveBlah(s"$fieldRef !") should be(s"$ident = :$ident")
+      //
+      resolveBlah(fieldRef, b) should be(s"base.$ident = :$ident?")
+      resolveBlah(s"$fieldRef!", b) should be(s"base.$ident = :$ident")
+      resolveBlah(s"$fieldRef !", b) should be(s"base.$ident = :$ident")
+      //
+      resolveBlahA(fieldRef, b) should be(s"ba.$ident = :$ident?")
+      resolveBlahA(s"$fieldRef!", b) should be(s"ba.$ident = :$ident")
+      resolveBlahA(s"$fieldRef !", b) should be(s"ba.$ident = :$ident")
+    }
+  }
+
+  "filter sugar syntax" should "work properly for valid field refs with expression" in {
+    val expr = fBlahExpr.expression
+    FieldRefs foreach { fieldRef =>
+      val ident = fieldRef.substring(1)
+      resolveBlahExpr(fieldRef) should be(s"$expr = :$ident?")
+      resolveBlahExpr(s"$fieldRef!") should be(s"$expr = :$ident")
+      resolveBlahExpr(s"$fieldRef !") should be(s"$expr = :$ident")
+      //
+      resolveBlahExpr(fieldRef, b) should be(s"$expr = :$ident?")
+      resolveBlahExpr(s"$fieldRef!", b) should be(s"$expr = :$ident")
+      resolveBlahExpr(s"$fieldRef !", b) should be(s"$expr = :$ident")
+    }
+  }
+
+  "expression substitution" should "work properly for field refs" in {
+    def filterExpr(s: String) = s"transform_me($s) = :'mypar'"
+    val fieldName = fBlah.name
+    val fieldRef = FieldRefs.head
+    val fieldExpr = fBlahExpr.expression
+
+    resolve(filterExpr(fieldRef)) should be(filterExpr(fieldRef))
+    resolveBlah(filterExpr(fieldRef)) should be(filterExpr(fieldName))
+    resolveBlahExpr(filterExpr(fieldRef)) should be(filterExpr(fieldExpr))
   }
 
   "filter sugar syntax" should "handle naming correctly" in {
