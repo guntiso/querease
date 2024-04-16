321d2308756e55d4d2e5af5b6673e37644a27346
Guntis Ozols <guntiso@uniso.lv>
2024-04-16 14:05:06 +0300

    mv-produced helper utility for test development

---
+++ mv-produced.cmd
@@ -0,0 +1,2 @@
+mv test/metadata/tresql-table-metadata-out-produced.yaml    test/metadata/tresql-table-metadata-out.yaml
+mv test/dtos/dtos-out-produced.scala                        test/dtos/dtos-out.scala
