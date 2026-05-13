70a20e080c9a96b3ac82d3bac8a087e34798b22f
Guntis Ozols <guntiso@uniso.lv>
2026-05-14 02:13:58 +0300

    mojoz 7.1.0-SNAPSHOT

--- src/QuereaseMetadata.scala
+++ src/QuereaseMetadata.scala
@@ -36,7 +36,7 @@ trait QuereaseMetadata {
 
   protected lazy val parserCacheSize: Int = 32 * 1024
   protected lazy val yamlMetadata = YamlMd.fromPaths()
-  lazy val metadataConventions: MdConventions = new SimplePatternMdConventions(resourceLoader)
+  lazy val metadataConventions: MdConventions = new SimplePatternMdConventions(resourceClassLoader)
   lazy val typeDefs: Seq[TypeDef] = TypeMetadata.customizedTypeDefs
   /** db connection name mapping to db instance */
   lazy val aliasToDb: Map[String, String] = QuereaseMetadata.aliasToDb(resourceLoader)
